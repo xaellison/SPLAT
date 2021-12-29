9d09e362c7e388bef6fb2f36de198397b702264b
xaellison <xaellison@gmail.com>
2021-12-28 22:29:06 -0500

    interpolated normals and sep ad non_ad

---
+++ src/scenes/menger.jl
@@ -0,0 +1,66 @@
+# RUN FROM /
+using Revise
+include("../geo.jl")
+include("../skys.jl")
+include("../tracer.jl")
+
+function main()
+
+    obj_path = "objs/smol_menger.obj"
+    tris = mesh_to_Tri(load(obj_path))
+	centroid = _centroid(tris)
+	println(centroid)
+	println(model_box(tris))
+    #tris = parse_obj(obj_path)
+    @info "$(length(tris)) triangles"
+    width = 512
+    height = Int(width * 3 / 4)
+    frame_n = 720
+
+	function moving_camera(frame_i, frame_n)
+		camera_pos = V3((10, 0, 0)) + centroid
+		look_at = zero(V3)
+		up = V3((0.0, 0.0, -1.0))
+		FOV =  45.0 * pi / 180.0
+
+		return get_camera(camera_pos, look_at, up, FOV)
+	end
+
+    depth = 9
+    dλ = 30
+    ITERS = 16
+
+    skys = [sky_stripes]
+    Threads.@threads for i = 1:frame_n
+
+		R = rotation_matrix(V3(1, 1, 1), 2 * pi * i / frame_n)
+        #translate(t, v) = STri(t[1], t[2] - v, t[3] - v, t[4] - v, t[5:7]...)
+		#translate(t, v) = STri(t[1], t[2] - v, t[3] - v, t[4] - v, t[5], t[6], t[7])
+		translate(t, v) = Tri(t[1], t[2] - v, t[3] - v, t[4] - v)
+
+		tris′ = map(t -> translate(t, -centroid), tris)
+
+        tris′ = map(t -> map(v -> R * v, t), tris′)
+
+        images = @time ad_frame_matrix(
+            moving_camera,
+            width,
+            height,
+            tris′,
+            skys,
+            dλ,
+            depth,
+            ITERS,
+            Float32(2 * pi / 40 * i / frame_n),
+            rand,
+            Array
+        )
+        for s in keys(skys)
+            Makie.save("out/menger/$(s)/$(lpad(i, 3, "0")).png", images[s])
+        end
+
+    end
+    println("~fin")
+end
+
+main()
--- src/tracer.jl
+++ src/tracer.jl
@@ -31,7 +31,7 @@ end
 
 ## !! Hit computers for AD and non-AD Rays
 
-function next_hit(rays :: AbstractArray{AbstractRay}, n_tris :: AbstractArray{Tuple{I, T}}) where {I, T}
+function next_hit(rays :: AbstractArray{R}, n_tris :: AbstractArray{Tuple{I, T}}) where {R<:AbstractRay, I, T}
     dest = Array{Tuple{Float32, Int32, T}}(undef, size(rays))
     next_hit!(dest, rays, n_tris)
     return dest
@@ -129,24 +129,24 @@ function evolve_ray(r::ADRay, d_n_t, rndm)::ADRay
     if r.in_medium
         n2, n1 = n1, n2
     end
-    N = optical_normal(t, p)
+    N(λ) = optical_normal(t, p(r.λ))
     in_medium = false
 
-    refracts = can_refract(r.dir, N, n1(r.λ), n2(r.λ)) && rndm <= reflectance(r.dir, N, n1(r.λ), n2(r.λ))
+    refracts = can_refract(r.dir, N(r.λ), n1(r.λ), n2(r.λ)) && rndm <= reflectance(r.dir, N(r.λ), n1(r.λ), n2(r.λ))
     in_medium = refracts ? !r.in_medium : r.in_medium
 
     if refracts
         return ADRay(p(r.λ),
                      ForwardDiff.derivative(p, r.λ),
-                     refract(r.dir, N, n1(r.λ), n2(r.λ)),
-                     ForwardDiff.derivative(λ -> refract(r.dir + r.dir′ * (λ - r.λ), N, n1(λ), n2(λ)), r.λ),
+                     refract(r.dir, N(r.λ), n1(r.λ), n2(r.λ)),
+                     ForwardDiff.derivative(λ -> refract(r.dir + r.dir′ * (λ - r.λ), N(r.λ), n1(λ), n2(λ)), r.λ),
                      in_medium, n, r.dest, r.λ)
 
     else
         return ADRay(p(r.λ),
                      ForwardDiff.derivative(p, r.λ),
-                     reflect(r.dir, N),
-                     ForwardDiff.derivative(λ -> reflect(r.dir + r.dir′ * (λ - r.λ), N), r.λ),
+                     reflect(r.dir, N(r.λ)),
+                     ForwardDiff.derivative(λ -> reflect(r.dir + r.dir′ * (λ - r.λ), N(λ)), r.λ),
                      in_medium, n, r.dest, r.λ)
     end
 
@@ -158,7 +158,7 @@ end
 
 ## Wrap it all up
 
-function frame_matrix(
+function ad_frame_matrix(
     camera_generator::Function,
     width::Int,
     height::Int,
@@ -213,8 +213,7 @@ function frame_matrix(
     I = nothing
     s0 = nothing
     #@showprogress for (iter, λ) in [(iter, λ) for iter = 1:ITERS for λ = λ_min:dλ:λ_max]
-    #@showprogress 
-    for iter = 1:ITERS
+    @showprogress for iter = 1:ITERS
 
         if has_run
             dv .=
@@ -283,6 +282,149 @@ function frame_matrix(
         end
 
         has_run = true
+    end
+
+    out = Dict()
+    for s in keys(skys)
+        _R, _G, _B = R[s], G[s], B[s]
+        img = Array{RGBf}(undef, size(_R)...)
+        @simd for i = 1:length(img)
+            if isnan(_R[i]) || isnan(_G[i]) || isnan(_B[i])
+                img[i] = RGBf(1.0f0, 0.0f0, 0.0f0)
+            else
+                r, g, b = clamp(_R[i], 0, 1), clamp(_G[i], 0, 1), clamp(_B[i], 0, 1)
+                img[i] = RGBf(r, g, b)
+            end
+        end
+        out[s] = img
+    end
+    return out
+end
+
+
+
+function frame_matrix(
+    camera_generator::Function,
+    width::Int,
+    height::Int,
+    tris,
+    skys,
+    dλ,
+    depth,
+    ITERS,
+    phi,
+    random,
+    A, # type: either Array or CuArray
+)
+    camera = camera_generator(1, 1)
+    R = Dict(s => zeros(Float32, height, width) for s in keys(skys))
+    G = Dict(s => zeros(Float32, height, width) for s in keys(skys))
+    B = Dict(s => zeros(Float32, height, width) for s in keys(skys))
+    #out .= RGBf(0, 0, 0)
+    λ_min = 400.0f0
+    λ_max = 700.0f0
+    intensity = Float32(1 / ITERS) * 2
+
+    function init_ray(x, y, λ, dv)::AbstractRay
+        _x, _y = x - width / 2, y - height / 2
+        scale = width * _COS_45 / camera.FOV_half_sin
+        _x /= scale
+        _y /= scale
+
+        _z = sqrt(1 - _x^2 - _y^2)
+        dir =
+            _x * camera.right +
+            _y * camera.up +
+            _z * camera.dir +
+            dv * 0.25f0 / max(width, height)
+        dir = normalize(dir)
+        idx = (x - 1) * height + y
+        polarization = normalize(cross(camera.up, dir))
+        return Ray(camera.pos, dir, polarization, false, 0, idx, λ)
+    end
+
+    I = map(Int32, collect(1:length(tris)))
+    n_tris = collect(zip(I, tris)) |> A |> m -> reshape(m, 1, length(m))
+
+    row_indices = A(1:width)
+    col_indices = reshape(A(1:height), 1, height)
+    has_run = false
+    rays = nothing
+    hits = nothing
+    grid = nothing
+    rndm = nothing
+    host_rays = nothing
+    dv = nothing
+    I = nothing
+    s0 = nothing
+    @showprogress for (iter, λ) in [(iter, λ) for iter = 1:ITERS for λ = λ_min:dλ:λ_max]
+
+        if has_run
+            dv .=
+                V3.(
+                    random(Float32, width),
+                    random(Float32, width),
+                    random(Float32, width),
+                )
+            rays .= reshape(init_ray.(row_indices, col_indices, λ, dv), width * height)
+        else
+            rndm = random(Float32, width * height)
+            dv =
+                V3.(
+                    random(Float32, width),
+                    random(Float32, width),
+                    random(Float32, width),
+                )
+            rays = reshape(init_ray.(row_indices, col_indices, λ, dv), width * height)
+        end
+
+        rndm .= random(Float32, width * height)
+        if has_run
+            next_hit!(hits, rays, n_tris)
+        else
+            hits = next_hit(rays, n_tris)#minimum(Broadcast.broadcasted(get_hit, n_tris, rays), dims=2, init=(Inf32, typemax(Int32), zero(STri)))
+        end
+        #	@info "tada"
+        for iter = 2:depth
+            rndm .= random(Float32, width * height)
+            #map!(d_r -> d_r[2], rays, hits)
+            map!(evolve_ray, rays, rays, hits, rndm)
+            hits = next_hit(rays, n_tris)#hits = minimum(Broadcast.broadcasted(get_hit, n_tris, rays), dims=2, init=(Inf32, typemax(Int32), zero(STri)))
+        end
+        rndm .= random(Float32, width * height)
+        map!(evolve_ray, rays, rays, hits, rndm)
+
+        if has_run
+            copyto!(host_rays, rays)
+        else
+            host_rays = Array(rays)
+        end
+
+        r0, g0, b0 = retina_red(λ), retina_green(λ), retina_blue(λ)
+        for (i, sky) in enumerate(skys)
+
+            g = r -> sky(r.dir, λ, phi)
+
+            if isnothing(I)
+                I = map(r->r.dest, host_rays)
+            else
+                map!(r->r.dest, I, host_rays)
+            end
+
+            if isnothing(s0)
+                s0 = g.(host_rays)
+            else
+                map!(g, s0, host_rays)
+            end
+
+            R[i][I] .+= intensity * s0 * r0 * dλ
+            G[i][I] .+= intensity * s0 * g0 * dλ
+            B[i][I] .+= intensity * s0 * b0 * dλ
+        end
+
+
+        has_run = true
+
     end
 
     out = Dict()
