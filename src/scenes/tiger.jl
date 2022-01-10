093e73c3790fcefb1a62653a245c62645ac7a3ac
xaellison <xaellison@gmail.com>
2022-01-09 19:46:35 -0500

    optimizations
    - for a single scene, create many images with changing background
    - sort! pseudo-ray queue (NB changes to evolver)

--- src/scenes/tiger.jl
+++ src/scenes/tiger.jl
@@ -7,7 +7,7 @@ include("../tracer.jl")
 include("../cuda.jl")
 function main()
 
-    obj_path = "objs/targetted_backless.obj"
+    obj_path = "objs/tiger_head.obj"
     tris = mesh_to_STri(load(obj_path))
 
 	centroid = _centroid(tris)
@@ -15,12 +15,12 @@ function main()
 	println(model_box(tris))
     #tris = parse_obj(obj_path)
     @info "$(length(tris)) triangles"
-    width = 512
-    height = 512#Int(width * 3 / 4)
+    width = 256
+    height = 256#Int(width * 3 / 4)
     frame_n = 720
 
 	function moving_camera(frame_i, frame_n)
-		camera_pos = V3((60, 0, 0))
+		camera_pos = V3((30, 0, 0))
 		look_at = zero(V3)
 		up = V3((0.0, 0.0, -1.0))
 		FOV =  45.0 * pi / 180.0
@@ -28,22 +28,22 @@ function main()
 		return get_camera(camera_pos, look_at, up, FOV)
 	end
 
-    depth = 4
+    depth = 3
     dλ = 10
     ITERS = 1
 
-    skys = [sky_stripes_down]
+    skys = [sky_curtains]
 
     #for i = 1:frame_n
 		# R0 gets corner up
 	#	R0 = rotation_matrix(cross(V3(1,1,0), V3(1, 1, 1)), pi/2-acos(vector_cosine(V3(1,1,1),V3(1,1,0))))
-		R = rotation_matrix(V3(1, 0, 0),  -pi)* rotation_matrix(V3(0, 0, 1),  -pi / 2 )# * R0
+		R = rotation_matrix(V3(0, 1, 0),  -pi / 6 ) * rotation_matrix(V3(0, 0, 1),  -pi / 2 )
         #translate(t, v) = STri(t[1], t[2] - v, t[3] - v, t[4] - v, t[5:7]...)
 		translate(t::STri, v) = STri(t[1], t[2] - v, t[3] - v, t[4] - v, t[5], t[6], t[7])
 		translate(t::Tri, v) = Tri(t[1], t[2] - v, t[3] - v, t[4] - v)
 
-		tris′ = map(t -> translate(t, -centroid ), tris)
-		println(model_box(tris′))
+		tris′ = map(t -> translate(t, -centroid + V3(0,0,-3)), tris)
+
         tris′ = map(t -> map(v -> R * v, t), tris′)
 
         images = @time ad_frame_matrix(
--- src/tracer.jl
+++ src/tracer.jl
@@ -146,8 +146,11 @@ end
 
 function evolve_ray(r::ADRay, d_n_t :: Tuple{Tuple{R, R}, I, T}, rndm)::ADRay where {R<:Real, I<:Integer, T}
     (d, d′), n, t = d_n_t
-    if isinf(d)
+    if r.retired
         return r
+    end
+    if isinf(d)
+        return retired(r)
     end
 
 
@@ -176,9 +179,7 @@ function ad_frame_matrix(
     A, # type: either Array or CuArray
 )
     camera = camera_generator(1, 1)
-    R = [A(zeros(Float32, height, width)) for s in skys]
-    G = [A(zeros(Float32, height, width)) for s in skys]
-    B = [A(zeros(Float32, height, width)) for s in skys]
+
     #out .= RGBf(0, 0, 0)
     λ_min = 400.0f0
     λ_max = 700.0f0
@@ -216,7 +217,8 @@ function ad_frame_matrix(
     I = nothing
     s0 = nothing
     #@showprogress for (iter, λ) in [(iter, λ) for iter = 1:ITERS for λ = λ_min:dλ:λ_max]
-    for iter = 1:ITERS
+    @info "tracing depth = $depth"
+    @time for iter = 1:ITERS
 
         if has_run
             dv .=
@@ -241,26 +243,43 @@ function ad_frame_matrix(
         if has_run
             next_hit!(hits, rays, n_tris, false)
         else
-            hits = next_hit(rays, n_tris, true)
+            CUDA.@time CUDA.@sync hits = next_hit(rays, n_tris, true)
         end
         #
         for iter = 2:depth
             rndm .= random(Float32, length(rays))
             #map!(d_r -> d_r[2], rays, hits)
             map!(evolve_ray, rays, rays, hits, rndm)
-            next_hit!(hits, rays, n_tris, false)
+            #if iter == 2
+            CUDA.@time CUDA.@sync     sort!(rays, by=ray->ray.retired)
+            cutoff = count(ray->!ray.retired, rays)
+            @info "cutoff $cutoff"
+            cutoff = min(length(rays), cutoff + 256 - cutoff % 256)
+                @info "cutoff $cutoff"
+            #end
+            h_view = @view hits[1:cutoff]
+            r_view = @view rays[1:cutoff]
+            CUDA.@time CUDA.@sync next_hit!(h_view, r_view, n_tris, false)
         end
         rndm .= random(Float32, width * height)
         map!(evolve_ray, rays, rays, hits, rndm)
+        has_run = true
+    end
 
-
-
-
+    frame_n = 180
+    @info "images"
+    R = A(zeros(Float32, height, width))
+    G = A(zeros(Float32, height, width))
+    B = A(zeros(Float32, height, width))
+    @time for frame_i in 1:frame_n
+        R .= 0.0f0
+        G .= 0.0f0
+        B .= 0.0f0
         for λ in λ_min:dλ:λ_max
             r0, g0, b0 = retina_red(λ), retina_green(λ), retina_blue(λ)
             for (i, sky) in enumerate(skys)
-
-                g = r -> r.in_medium ? 0.0f0 : sky(r.dir + r.dir′ * (λ - r.λ), λ, phi)
+                # WARNING deleted `r.in_medium ? 0.0f0 : `
+                g = r -> sky(r.dir + r.dir′ * (λ - r.λ), λ, Float32(2 * pi / 20 * frame_i / frame_n))
 
                 if isnothing(I)
                     I = map(r->r.dest, rays)
@@ -274,171 +293,28 @@ function ad_frame_matrix(
                     map!(g, s0, rays)
                 end
 
-                R[i][I] .+= intensity * s0 * r0 * dλ
-                G[i][I] .+= intensity * s0 * g0 * dλ
-                B[i][I] .+= intensity * s0 * b0 * dλ
+                R[I] .+= intensity * s0 * r0 * dλ
+                G[I] .+= intensity * s0 * g0 * dλ
+                B[I] .+= intensity * s0 * b0 * dλ
             end
         end
 
-        has_run = true
-    end
-
-    out = Dict()
-    for s in keys(skys)
-        _R, _G, _B = map(Array, (R[s], G[s], B[s]))
-        img = Array{RGBf}(undef, size(_R)...)
-        @simd for i = 1:length(img)
-            if isnan(_R[i]) || isnan(_G[i]) || isnan(_B[i])
-                img[i] = RGBf(1.0f0, 0.0f0, 0.0f0)
-            else
-                r, g, b = clamp(_R[i], 0, 1), clamp(_G[i], 0, 1), clamp(_B[i], 0, 1)
-                img[i] = RGBf(r, g, b)
-            end
-        end
-        out[s] = img
-    end
-    return out
-end
-
-
-
-function frame_matrix(
-    camera_generator::Function,
-    width::Int,
-    height::Int,
-    tris,
-    skys,
-    dλ,
-    depth,
-    ITERS,
-    phi,
-    random,
-    A, # type: either Array or CuArray
-)
-    camera = camera_generator(1, 1)
-    R = [zeros(Float32, height, width) for s in skys]
-    G = [zeros(Float32, height, width) for s in skys]
-    B = [zeros(Float32, height, width) for s in skys]
-    #out .= RGBf(0, 0, 0)
-    λ_min = 400.0f0
-    λ_max = 700.0f0
-    intensity = Float32(1 / ITERS) * 2
-
-    function init_ray(x, y, λ, dv)::AbstractRay
-        _x, _y = x - width / 2, y - height / 2
-        scale = width * _COS_45 / camera.FOV_half_sin
-        _x /= scale
-        _y /= scale
-
-        _z = sqrt(1 - _x^2 - _y^2)
-        dir =
-            _x * camera.right +
-            _y * camera.up +
-            _z * camera.dir +
-            dv * 0.25f0 / max(width, height)
-        dir = normalize(dir)
-        idx = (x - 1) * height + y
-        polarization = normalize(cross(camera.up, dir))
-        return Ray(camera.pos, dir, polarization, false, 0, idx, λ)
-    end
-
-    I = map(Int32, collect(1:length(tris)))
-    n_tris = collect(zip(I, tris)) |> A |> m -> reshape(m, 1, length(m))
-
-    row_indices = A(1:width)
-    col_indices = reshape(A(1:height), 1, height)
-    has_run = false
-    rays = nothing
-    hits = nothing
-    grid = nothing
-    rndm = nothing
-    host_rays = nothing
-    dv = nothing
-    I = nothing
-    s0 = nothing
-    @showprogress for (iter, λ) in [(iter, λ) for iter = 1:ITERS for λ = λ_min:dλ:λ_max]
-
-        if has_run
-            dv .=
-                V3.(
-                    random(Float32, width),
-                    random(Float32, width),
-                    random(Float32, width),
-                )
-            rays .= reshape(init_ray.(row_indices, col_indices, λ, dv), width * height)
-        else
-            rndm = random(Float32, width * height)
-            dv =
-                V3.(
-                    random(Float32, width),
-                    random(Float32, width),
-                    random(Float32, width),
-                )
-            rays = reshape(init_ray.(row_indices, col_indices, λ, dv), width * height)
-        end
-
-        rndm .= random(Float32, width * height)
-        if has_run
-            next_hit!(hits, rays, n_tris)
-        else
-            hits = next_hit(rays, n_tris)#minimum(Broadcast.broadcasted(get_hit, n_tris, rays), dims=2, init=(Inf32, typemax(Int32), zero(STri)))
-        end
-        #
-        for iter = 2:depth
-            rndm .= random(Float32, width * height)
-            #map!(d_r -> d_r[2], rays, hits)
-            map!(evolve_ray, rays, rays, hits, rndm)
-            hits = next_hit(rays, n_tris)#hits = minimum(Broadcast.broadcasted(get_hit, n_tris, rays), dims=2, init=(Inf32, typemax(Int32), zero(STri)))
-        end
-        rndm .= random(Float32, width * height)
-        map!(evolve_ray, rays, rays, hits, rndm)
-
-        if has_run
-            copyto!(host_rays, rays)
-        else
-            host_rays = Array(rays)
-        end
-
-        r0, g0, b0 = retina_red(λ), retina_green(λ), retina_blue(λ)
-        for (i, sky) in enumerate(skys)
-
-            g = r -> sky(r.dir, λ, phi)
-
-            if isnothing(I)
-                I = map(r->r.dest, host_rays)
-            else
-                map!(r->r.dest, I, host_rays)
+        out = Dict()
+        for s in keys(skys)
+            _R, _G, _B = map(Array, (R, G, B))
+            img = Array{RGBf}(undef, size(_R)...)
+            @simd for i = 1:length(img)
+                if isnan(_R[i]) || isnan(_G[i]) || isnan(_B[i])
+                    img[i] = RGBf(1.0f0, 0.0f0, 0.0f0)
+                else
+                    r, g, b = clamp(_R[i], 0, 1), clamp(_G[i], 0, 1), clamp(_B[i], 0, 1)
+                    img[i] = RGBf(r, g, b)
+                end
             end
 
-            if isnothing(s0)
-                s0 = g.(host_rays)
-            else
-                map!(g, s0, host_rays)
-            end
+            Makie.save("out/tiger/_/$(lpad(frame_i, 3, "0")).png", img)
 
-            R[i][I] .+= intensity * s0 * r0 * dλ
-            G[i][I] .+= intensity * s0 * g0 * dλ
-            B[i][I] .+= intensity * s0 * b0 * dλ
         end
-
-
-        has_run = true
-
     end
-
-    out = Dict()
-    for s in keys(skys)
-        _R, _G, _B = R[s], G[s], B[s]
-        img = Array{RGBf}(undef, size(_R)...)
-        @simd for i = 1:length(img)
-            if isnan(_R[i]) || isnan(_G[i]) || isnan(_B[i])
-                img[i] = RGBf(1.0f0, 0.0f0, 0.0f0)
-            else
-                r, g, b = clamp(_R[i], 0, 1), clamp(_G[i], 0, 1), clamp(_B[i], 0, 1)
-                img[i] = RGBf(r, g, b)
-            end
-        end
-        out[s] = img
-    end
-    return out
+    return nothing
 end
