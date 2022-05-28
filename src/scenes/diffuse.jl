3042b75350e6390ab836b5919e62842105089664
xaellison <xaellison@gmail.com>
2022-05-28 12:50:39 -0400

    update tri sphere scene

--- src/scenes/diffuse.jl
+++ src/scenes/diffuse.jl
@@ -1,13 +1,22 @@
-# RUN FROM /
-using Revise, CUDA, LazyArrays
 
+
+# RUN FROM /
+using Revise, CUDA, LazyArrays#, GLMakie
+import CUDA.NVTX.@range
 include("../geo.jl")
 include("../skys.jl")
 include("../tracer.jl")
 include("../cuda.jl")
 function main()
-	frame_n = 120
-	for frame_i in 1:frame_n
+
+    width = 896
+    height = 896
+    xmin = 1
+    xmax = height
+    ymin = 1
+    ymax = width
+
+
 	translate(t :: Tri, v) = Tri(t[1], t[2] + v, t[3] + v, t[4] + v, t[5], t[6], t[7])
 	translate(t :: STri, v) = STri(t[1], t[2] + v, t[3] + v, t[4] + v, t[5], t[6], t[7])
 	translate(t :: FTri, v) = FTri(t[1], t[2] + v, t[3] + v, t[4] + v, t[5], t[6], t[7], t[8], t[9], t[10])
@@ -16,66 +25,123 @@ function main()
 	tris = mesh_to_FTri(load(obj_path))
 	c = _centroid(tris)
 	map!(t->translate(t, V3(3.0, 0, 0.0) - c), tris, tris)
-	V = V3(0, 2, 3)
+	V = V3(0, cos(2 * pi ), sin(2 * pi )) * 0.6
 	meshes = [tris]
-	for i in -2:3
-		tris = mesh_to_FTri(load(obj_path))
-		c = _centroid(tris)
-		map!(t->translate(t, V3(-3.0, 0, 0.0) + V * (i - (frame_i-1)/( frame_n - 1)) - c), tris, tris)
-		push!(meshes, tris)
-	end
+	tris = mesh_to_FTri(load(obj_path))
+	c = _centroid(tris)
+	map!(t->translate(t, V3(-3.0, 0, 0.0) + V - c), tris, tris)
+	push!(meshes, tris)
 	tris = foldl(vcat, meshes)
-
-	centroid = _centroid(tris)
-	println(centroid)
-	println(model_box(tris))
-    #tris = parse_obj(obj_path)
-    @info "$(length(tris)) triangles"
-    width = 1024
-    height = 1024#Int(width * 3 / 4)
+	n_tris = collect(zip(map(Int32, collect(1:length(tris))), tris)) |>
+		m -> reshape(m, 1, length(m))
 
 
-	function moving_camera(frame_i, frame_n)
-		camera_pos = V3((7, 0, 0)) #+ centroid
-		look_at = zero(V3)
-		up = V3((0.0, 0.0, -1.0))
-		FOV =  45.0 * pi / 180.0
 
-		return get_camera(camera_pos, look_at, up, FOV)
-	end
-
-    depth = 3
+    x = collect(xmin:xmax)#LinRange(-2, 1, 200)
+    y = collect(ymin:ymax)#LinRange(-1.1, 1.1, 200)
+    function init(x, y)
+        RGBf(rand(), rand(), rand())
+    end
+    img = init.(x, y')
+    #fig, ax, hm = image(x, y, img)
+    #display(fig)
     dλ = 25.0f0
-    ITERS = 1
+    λ_min = 400.0f0
+    λ_max = 700.0f0
+    RGB3 = CuArray{Float32}(undef, width * height, 3)
+    RGB = CuArray{RGBf}(undef, width * height)
 
-    skys = [sky_stripes_down]
+    row_indices = CuArray(1:height)
+    col_indices = reshape(CuArray(1:width), 1, width)
+    rays = CuArray{ADRay}(undef, width * height)
+    hit_idx = CuArray(zeros(Int32, length(rays)))
+    dv = CuArray{V3}(undef, height) # make w*h
+    s0 = CuArray{Float32}(undef, length(rays), 3)
 
 
-		R = rotation_matrix(V3(1, 1, 1), 2 * pi * 0 / frame_n)
+    # use host to compute constants used in turning spectra into colors
+    spectrum = collect(λ_min:dλ:λ_max) |> a -> reshape(a, 1, 1, length(a))
+    retina_factor = Array{Float32}(undef, 1, 3, length(spectrum))
+    map!(retina_red, begin
+        @view retina_factor[1, 1, :]
+    end, spectrum)
+    map!(retina_green, begin
+        @view retina_factor[1, 2, :]
+    end, spectrum)
+    map!(retina_blue, begin
+        @view retina_factor[1, 3, :]
+    end, spectrum)
 
-		tris′ = map(t -> translate(t, V3(0,0,0) ), tris)
-		println(model_box(tris′))
-        #tris′ = map(t -> map(v -> R * v, t), tris′)
-		hit_tris = map(t->Tri(t[1:4]...), tris′)
+    retina_factor = CuArray(retina_factor)
+    spectrum = CuArray(spectrum)
 
-		images = @time ad_frame_matrix(
+    # Datastruct init
+    expansion = CuArray{FastRay}(undef, (length(rays), 1, length(spectrum)))
+    hits = CuArray{Int32}(undef, size(expansion))
+    tmp = CuArray{Tuple{Float32, Int32}}(undef, size(expansion))
+    rndm = CUDA.rand(Float32, height * width)
+    θ = 0.0f0
+    host_RGB = nothing
+    for ffff in 1:4
+    @time begin
+        θ += 2 * π / 360
+        function moving_camera(frame_i, frame_n)
+            camera_pos = V3((7, 0, 0)) #+ centroid
+            look_at = zero(V3)
+            up = V3((0.0, 0.0, -1.0))
+            FOV = 45.0 * pi / 180.0
+
+            return get_camera(camera_pos, look_at, up, FOV)
+        end
+
+        tris = CuArray(tris)
+        n_tris = CuArray(n_tris)
+        depth = 2
+        ITERS = 1
+
+        skys = [sky_stripes_down]
+        ad_frame_matrix(
             moving_camera,
-            width,
             height,
-			hit_tris,
-            tris′,
-            skys,
+            width,
             dλ,
             depth,
             ITERS,
             0,
             CUDA.rand,
-            CuArray,
-			false,
-			"diffuse/may/$(lpad(frame_i, 3, "0"))",
-			length(mesh_to_FTri(load(obj_path))) + 1
+            false,
+            3;
+            RGB3 = RGB3,
+            RGB=RGB,
+            n_tris = n_tris,
+            tris = tris,
+            row_indices = row_indices,
+            col_indices = col_indices,
+            rays = rays,
+            hit_idx = hit_idx,
+            dv = dv,
+            s0 = s0,
+
+            # Datastruct init
+            expansion = expansion,
+            hits = hits,
+            rndm = rndm,
+            tmp=tmp,
+            # use host to compute constants used in turning spectra into colors
+            spectrum = spectrum,
+            retina_factor = retina_factor,
         )
-	end
+        if isnothing(host_RGB)
+            host_RGB = Array(RGB)
+        else
+            copyto!(host_RGB, RGB)
+        end
+        #hm[3] = reshape(host_RGB, (height,width))
+    end
+    #    yield()
+        #title = "pure/may/$(lpad(frame_i, 3, "0"))"
+        #Makie.save("out/$title.png", img)
+    end
     println("~fin")
 end
 
