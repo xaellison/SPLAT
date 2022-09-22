5ebe7b74705ebff6cf0b581cf6388c6a0aa9b4c3
xaellison <25183646+xaellison@users.noreply.github.com>
2022-09-22 19:58:41 -0400

    move get_hit back into tracer

--- src/tracer.jl
+++ src/tracer.jl
@@ -12,6 +12,68 @@ using Serialization
 
 import Random.rand!
 
+## get_hit methods - how to intersect rays with geometric elements
+
+function get_hit(i_S::Tuple{Int32,Sphere}, r::AbstractRay; kwargs...)
+    # https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
+    i, S = i_S
+    if S.radius <= 0
+        return (Inf32, one(Int32), S)
+    end
+    t = distance_to_sphere(r.pos, r.dir, S)
+    if isinf(t)
+        return (Inf32, one(Int32), S)
+    end
+    return (t, i, S)
+end
+
+function get_hit(i_S::Tuple{Int32,Sphere}, r::ADRay; kwargs...)
+    i, S = i_S
+    if S.radius <= 0
+        return ((Inf32, Inf32), one(Int32), S)
+    end
+    t0 = distance_to_sphere(r.pos, r.dir, S)
+    t(λ) = distance_to_sphere(r.pos + (λ - r.λ) * r.pos′, r.dir + (λ - r.λ) * r.dir′, S)
+    if isinf(t0)
+        return ((Inf32, Inf32), one(Int32), S)
+    end
+    return ((t0, ForwardDiff.derivative(t, r.λ)), i, S)
+end
+
+function get_hit(
+    i_T::Tuple{Int32,AbstractTri},
+    r::AbstractRay;
+    unsafe = false,
+)::Tuple{Float32,Int32,AbstractTri} where {AbstractTri}
+    # unsafe = true will ignore the in triangle test: useful for continuum_shade
+    i, T = i_T
+    t = distance_to_plane(r, T)
+    p = r.pos + r.dir * t
+    if (unsafe || in_triangle(p, T)) && t > 0 && r.ignore_tri != i
+        return (t, i, T) # formerly d, n, t
+    else
+        return (Inf32, one(Int32), T)
+    end
+end
+
+function get_hit(
+    i_T::Tuple{Int32,AbstractTri},
+    r::ADRay;
+    kwargs...,
+)::Tuple{Tuple{Float32,Float32},Int32,AbstractTri} where {AbstractTri}
+    i, T = i_T
+    # In the case of i = 1, the degenerate triangle, this will be NaN.
+    # t0 = NaN fails d0 > 0 below, which properly gives us i = 1 back
+    t0 = distance_to_plane(r, T)
+    t(λ) = distance_to_plane(r, T, λ)
+    p = r.pos + r.dir * t0
+    if in_triangle(p, T) && t0 > 0 && r.ignore_tri != i
+        return ((t0, ForwardDiff.derivative(t, r.λ)), i, T)
+    else
+        return ((Inf32, Inf32), one(Int32), T)
+    end
+end
+
 
 ## Ray evolvers
 
