b012643edd77ec6afcfb489828d7d1d3edf45ee7
xaellison <25183646+xaellison@users.noreply.github.com>
2022-09-28 12:01:58 -0400

    clean and reorg imagers
    removed deprecated and or inconsistent funcs

--- src/ray_imagers.jl
+++ src/ray_imagers.jl
@@ -1,112 +1,5 @@
-# Functions that take a set of evolved rays and convert them to image output
-# (including light-map textures)
 
-# The first group is shaders - given a ray, wavelength, hit: return intensity
-
-function shade(r::FastRay, n, t, first_diffuse_index)::Float32
-    # NB disregards in_medium
-    # evolve to hit a diffuse surface
-    d, n, t = get_hit((n, t), r)
-    r = FastRay(r.pos + r.dir * d, zero(ℜ³), r.ignore_tri)
-
-    if n >= first_diffuse_index
-        # compute the position in the new triangle, set dir to zero
-        u, v = reverse_uv(r.pos, t)
-        s = 0.5f0
-        if xor(u % s > s / 2, v % s > s / 2)
-            return 1.0f0
-        else
-            return 0.0f0
-        end
-    end
-    if isinf(d)
-        return 0.0f0
-    end
-    return 0.0f0
-end
-
-function shade(adr::ADRay, n, t, first_diffuse_index, λ)::Float32
-    # NB disregards in_medium
-    # evolve to hit a diffuse surface
-    r = expand(adr, λ)
-    d, n, t = get_hit((n, t), r)
-    r = FastRay(r.pos + r.dir * d, zero(ℜ³), r.ignore_tri)
-
-    if n >= first_diffuse_index
-        # compute the position in the new triangle, set dir to zero
-        u, v = reverse_uv(r.pos, t)
-        s = 0.5f0
-        if xor(u % s > s / 2, v % s > s / 2)
-            return 1.0f0
-        else
-            return 0.0f0
-        end
-    end
-    if isinf(d)
-        return 0.0f0
-    end
-    return 0.0f0
-end
-
-
-function shade_tex(
-    r::FastRay,
-    n,
-    t,
-    first_diffuse_index,
-    tex::AbstractArray{Float32},
-)::Float32
-    # NB disregards in_medium
-    # evolve to hit a diffuse surface
-    d, n, t = get_hit((n, t), r)
-    r = FastRay(r.pos + r.dir * d, r.dir, r.ignore_tri)
-
-    if n >= first_diffuse_index
-        # compute the position in the new triangle, set dir to zero
-        u, v = tex_uv(r.pos, t)
-        CUDA.@assert !isnan(u) && !isnan(v)
-        # it's theoretically possible u, v could come back as zero
-        i = clamp(Int(ceil(u * (size(tex)[1]))), 1, size(tex)[1])
-        j = clamp(Int(ceil(v * (size(tex)[2]))), 1, size(tex)[2])
-        return tex[i, j] * cosine_shading(r, t)
-    end
-    if isinf(d)
-        return 0.0f0
-    end
-    return 0.0f0
-end
-
-function shade_tex(
-    adr::ADRay,
-    n,
-    t,
-    first_diffuse_index,
-    λ, δx, δy,
-    i_λ,
-    tex::AbstractArray{Float32},
-)::Float32
-    # NB disregards in_medium
-    # evolve to hit a diffuse surface
-    r = expand(adr, λ, δx, δy)
-    d, n, t = get_hit((n, t), r, unsafe = true)
-    # WARNING inconsistent ray defined
-    r = FastRay(r.pos + r.dir * d, r.dir, r.ignore_tri)
-
-    if n >= first_diffuse_index
-        # compute the position in the new triangle, set dir to zero
-        u, v = tex_uv(r.pos, t)
-
-        CUDA.@assert !isnan(u) && !isnan(v)
-        # it's theoretically possible u, v could come back as zero
-        i = clamp(Int(ceil(u * (size(tex)[1]))), 1, size(tex)[1])
-        j = clamp(Int(ceil(v * (size(tex)[2]))), 1, size(tex)[2])
-        return tex[i, j, i_λ] * cosine_shading(r, t)
-    end
-    if isinf(d)
-        return 0.0f0
-    end
-    return 0.0f0
-end
+## Light mapping
 
 function atomic_spectrum_kernel(
     rays::AbstractArray{ADRay},
@@ -150,40 +43,6 @@ function atomic_spectrum_kernel(
 end
 
 
-function atomic_spectrum_kernel(
-    rays::AbstractArray{FastRay},
-    hits,
-    tris,
-    first_diffuse_index,
-    spectrum,
-    tex,
-    n_λ,
-)
-    idx = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    r = rays[idx]
-    n = hits[idx]
-    t = tris[idx]
-    if n >= first_diffuse_index
-        d, n, t = get_hit((n, t), r)
-        # NOTE this r breaks convention of dir being zero for retired rays
-        r = FastRay(r.pos + r.dir * d, r.dir, r.ignore_tri)
-        u, v = tex_uv(r.pos, t)
-
-        if !isnan(u) && !isnan(v) && !isinf(u) && !isinf(v)
-            # it's theoretically possible u, v could come back as zero
-            w, h = size(tex)[1:2]
-            i = clamp(Int(ceil(u * w)), 1, w)
-            j = clamp(Int(ceil(v * h)), 1, h)
-
-            intensity = cosine_shading(r, t)
-            CUDA.@atomic tex[i, j, n_λ] += intensity
-        end
-    end
-    nothing
-end
-
-# the second group is functions that invoke a kernel of a group one function
-
 function continuum_light_map!(;
     tracer,
     tris,
@@ -210,40 +69,38 @@ function continuum_light_map!(;
     )
 end
 
-function expansion_light_map!(;
-    tris,
-    hit_idx,
-    tmp,
-    rays,
-    expansion,
-    n_tris,
-    spectrum,
-    first_diffuse,
-    dλ,
-    tex,
-    kwargs...,
-)
-    for (n_λ, λ) in enumerate(Array(spectrum))
-        expansion .= expand.(rays, λ)
-        hit_idx .= Int32(1)
-        next_hit!(hit_idx, tmp, expansion, n_tris)
-        tri_view = @view tris[hit_idx]
+## Final image rendering
 
+function shade_tex(
+    adr::ADRay,
+    n,
+    t,
+    first_diffuse_index,
+    λ, δx, δy,
+    i_λ,
+    tex::AbstractArray{Float32},
+)::Float32
+    # NB disregards in_medium
+    # evolve to hit a diffuse surface
+    r = expand(adr, λ, δx, δy)
+    d, n, t = get_hit((n, t), r, unsafe = true)
+    # WARNING inconsistent ray defined
+    r = FastRay(r.pos + r.dir * d, r.dir, r.ignore_tri)
 
-        @assert length(rays) % 256 == 0
-        @assert length(rays) == length(hit_idx) == length(tri_view)
+    @inbounds if n >= first_diffuse_index
+        # compute the position in the new triangle, set dir to zero
+        u, v = tex_uv(r.pos, t)
 
-        @cuda blocks = length(rays) ÷ 256 threads = 256 atomic_spectrum_kernel(
-            expansion,
-            hit_idx,
-            tri_view,
-            first_diffuse,
-            spectrum,
-            tex,
-            n_λ,
-        )
+        CUDA.@assert !isnan(u) && !isnan(v)
+        # it's theoretically possible u, v could come back as zero
+        i = clamp(Int(ceil(u * (size(tex)[1]))), 1, size(tex)[1])
+        j = clamp(Int(ceil(v * (size(tex)[2]))), 1, size(tex)[2])
+        return tex[i, j, i_λ] * cosine_shading(r, t)
     end
-    synchronize()
+    if isinf(d)
+        return 0.0f0
+    end
+    return 0.0f0
 end
 
 
@@ -284,42 +141,5 @@ function continuum_shade!(imager::StableImager;
 
     map!(brightness -> clamp(brightness, 0, 1), RGB3, RGB3)
 
-    RGB .= RGBf.(RGB3[:, 1], RGB3[:, 2], RGB3[:, 3])
-end
-
-
-function expansion_shade!(;
-    RGB3,
-    RGB,
-    tris,
-    hit_idx,
-    tmp,
-    rays,
-    n_tris,
-    spectrum,
-    expansion,
-    first_diffuse,
-    retina_factor,
-    intensity = 7.0f-2,
-    dλ,
-    tex,
-    kwargs...,
-)
-    RGB3 .= 0.0f0
-    for (n_λ, λ) in enumerate(Array(spectrum))
-        expansion .= expand.(rays, λ)
-        hit_idx .= Int32(1)
-        next_hit!(hit_idx, tmp, expansion, n_tris)
-        tri_view = @view tris[hit_idx]
-
-        tex_view = @view tex[:, :, n_λ]
-        s(args...) = shade_tex(args..., tex_view)
-        broadcast = @~ s.(rays, hit_idx, tri_view, first_diffuse, spectrum[:, :, n_λ]) .*
-           retina_factor[:, :, n_λ] .* intensity .* dλ
-        # broadcast rule not implemeted for sum!
-        RGB3 .+= sum(broadcast, dims = 3) |> a -> reshape(a, length(rays), 3)
-    end
-
-    map!(brightness -> clamp(brightness, 0, 1), RGB3, RGB3)
     RGB .= RGBf.(RGB3[:, 1], RGB3[:, 2], RGB3[:, 3])
 end
