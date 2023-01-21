fe8293575fd356848b1a033f2e2afa6ab8023c3f
xaellison <25183646+xaellison@users.noreply.github.com>
2023-01-21 18:28:59 -0500

    atomic argmin replace arithmetic with bitwise

--- src/atomic_argmin.jl
+++ src/atomic_argmin.jl
@@ -1,73 +1,31 @@
 # Linearly encode float32s into uint32 in a way that preserves comparison & equality
 # for all normal non-NaN values
 
-@inline function unsafe_encode_parts(f::Float32)::Tuple{UInt32,UInt32,UInt32}
-    if f == 0.0f0
-        # greater than any negative, but less than any positive since significand
-        # for any non-zero number is 1.x
-        return UInt32(1 << 31), zero(UInt32), zero(UInt32)
-    end
-    if f == Inf32
-        return typemax(UInt32), zero(UInt32), zero(UInt32)
-    end
-    if f == -Inf32
-        return typemin(UInt32), zero(UInt32), zero(UInt32)
-    end
-    if f > 0
-        sign = UInt32(1 << 31)
-        # https://en.wikipedia.org/wiki/Single-precision_floating-point_format
-        # See how exponent is interpretted as exponent - 127
-        # TODO: can handle subnormal nums by identifying when exponent(f) < -127
-        exp = (exponent(f) + 127) * 1 << 23 |> UInt32
-        sig = (significand(f) - 1) * 1 << 23 |> UInt32
-    else
-        # for negative numbers, larger exponent = more negative
-        sign = zero(UInt32)
-        exp = (127 - exponent(f)) * 1 << 23 |> UInt32
-        # significand is signed. Again, effectively subtract int-string from max value
-        sig = (2 + significand(f)) * 1 << 23 |> UInt32
-    end
-
-    return sign, exp, sig
-end
 
 function unsafe_encode(f::Float32)::UInt32
-    (sign, exp, sig) = unsafe_encode_parts(f)
-    return sign + exp + sig
+    # equivalent with float comparison
+    #f < 0 ? ~reinterpret(UInt32, f) : reinterpret(UInt32, f) + UInt32(1<<31)
+    uint = reinterpret(UInt32, f)
+    uint > 1 << 31 ? ~uint : uint | UInt32(1 << 31)
 end
 
 function unsafe_encode(f::Float32, i::UInt32)::UInt64
-    encoded_f = UInt64(unsafe_encode(f))
-    return encoded_f * 1 << 32 + i
+    return UInt64(unsafe_encode(f)) << 32 | UInt64(i)
 end
 
 function unsafe_decode(i64::UInt64)::UInt32
     #TODO rename to retreive or something
-    return UInt32(i64 % 1 << 32)
-end
-
-function unsafe_decode_parts(i32::UInt32)
-    sign = i32 ÷ (1 << 31)
-    exp = i32 % (1 << 31) ÷ (1 << 23)
-    sig = i32 % (1 << 23)
-    return sign, exp, sig
+    return UInt32(i64 ⊻ (i64 >> 32 << 32))
 end
 
 function unsafe_decode(i32::UInt32)::Float32
-    u_sign, u_exp, u_sig = unsafe_decode_parts(i32)
-    if (u_sign, u_exp, u_sig) == (1, 0, 0)
-        # special case avoids 0.0f0 -> 5.877472f-39
-        return 0.0f0
-    end
-    if u_sign > 0
-        exp = Int32(u_exp) - 127
-        sig = Float32(u_sig) / (1 << 23) + 1
-        return 2.0f0^exp * sig
+    flipped_sign = i32 >> 31 << 31
+    if flipped_sign == 0
+        # originally, sign 0 positive, so this is negative, restore
+        return reinterpret(Float32, ~i32)
     else
-        exp = 127 - Int32(u_exp)
-        sig = Float32(u_sig) / (1 << 23) - 2
-        return 2.0f0^exp * sig
-    end
+        return reinterpret(Float32, i32 ⊻ UInt32(1 << 31))
+    end 
 end
 
 function test_array()
@@ -121,7 +79,7 @@ function test_comparison2()
         e_B = map(unsafe_decode, B)
         for (x, e_x) in zip(A, e_A)
             for (y, e_y) in zip(B, e_B)
-                if (x < y) != (e_x < e_y)
+                if !isnan(e_x) & !isnan(e_y) & (x < y) != (e_x < e_y)
                     @error "Failed for $x < $y != $e_x < $e_y"
                     return false
                 end
@@ -139,4 +97,4 @@ end
 using BenchmarkTools
 @benchmark unsafe_encode(f) setup = begin
     f = -rand(Float32) .- 0.5f0
-end evals = 10 samples = 10
+end evals = 100 samples = 10
