5b7a73c248fb34d2fd86248a999a6bf4b53eb458
Alex Ellison <aellison@voltus.co>
2023-07-15 09:46:46 -0400

    pre pipeline refactor

--- src/scenes/lobes.jl
+++ src/scenes/lobes.jl
@@ -1,5 +1,5 @@
 # RUN FROM /
-using Revise, LazyArrays, Parameters, GLMakie, CUDA, KernelAbstractions, Random, NVTX
+using Revise, LazyArrays, Parameters, GLMakie, CUDA, Random, NVTX
 
 include("../geo.jl")
 include("../skys.jl")
@@ -7,133 +7,244 @@ include("../tracer.jl")
 include("../procedural_assets.jl")
 
 function main()
-	axis = ℜ³(0, 1, 0) + ℜ³(0.3, 0, 0)
-	out = nothing
-	width = 2048
-	height = 2048
- 	begin	# Tracing params
-	    dλ = 25f0
-	    λ_min = 400.0f0
-	    λ_max = 700.0f0
-	    depth = 5
-		forward_upscale = 2
-		backward_upscale = 2
-		iterations_per_frame=1
-		# Geometry
-		frame = 1
-		lobe1 = mesh_to_FTri(load("objs/lobe1_2.obj"))
-		lobe2 = mesh_to_FTri(load("objs/lobe2_2.obj"))
-		R = rotation_matrix(ℜ³(1,0,0), 2 * pi * frame / 180) * rotation_matrix(axis, 2 * pi * 44 / 180) * rotation_matrix(ℜ³(0, 0, 1), pi / 4) * rotation_matrix(ℜ³(1, 0, 0), pi / 2) * rotation_matrix(ℜ³(0, 0, 1), pi / 4)
-		lobe1 = map(t -> translate(t, ℜ³(0, 0, -0.25) ), lobe1)
-		lobe1 = map(t -> rotate(t, R), lobe1)
-		lobe2 = map(t -> translate(t, ℜ³(0, 0, -0.25) ), lobe2) 
-		lobe2 = map(t -> rotate(t, R), lobe2)
-		meshes = [[zero(FTri)], lobe1, lobe2]
-		first_diffuse = 1 + length(lobe1) + 1
-		tris = foldl(vcat, meshes)
+    axis = ℜ³(0, 1, 0) + ℜ³(0.3, 0, 0)
+    out = nothing
+    width = 1024
+    height = 1024
+    # Tracing params
+        dλ = 25.0f0
+        λ_min = 400.0f0
+        λ_max = 700.0f0
+        depth = 5
+        forward_upscale = 1
+        backward_upscale = 1
+        iterations_per_frame = 1
+        # Geometry
+        frame = 1
+        lobe1 = mesh_to_FTri(load("objs/lobe1_2.obj"))
+        lobe2 = mesh_to_FTri(load("objs/lobe2_2.obj"))
+        R =
+            rotation_matrix(ℜ³(1, 0, 0), 2 * pi * frame / 180) *
+            rotation_matrix(axis, 2 * pi * 44 / 180) *
+            rotation_matrix(ℜ³(0, 0, 1), pi / 4) *
+            rotation_matrix(ℜ³(1, 0, 0), pi / 2) *
+            rotation_matrix(ℜ³(0, 0, 1), pi / 4)
+        lobe1 = map(t -> translate(t, ℜ³(0, 0, -0.25)), lobe1)
+        lobe1 = map(t -> rotate(t, R), lobe1)
+        lobe2 = map(t -> translate(t, ℜ³(0, 0, -0.25)), lobe2)
+        lobe2 = map(t -> rotate(t, R), lobe2)
+        meshes = [[zero(FTri)], lobe1, lobe2]
+        first_diffuse = 1 + length(lobe1) + 1
+        tris = foldl(vcat, meshes)
 
-		tv = @view tris[2:end]
-		centroid = centroidish(tv)
-					
-		R = rotation_matrix(ℜ³(0,0,1),  pi * 0.16)
-		tv .= map(t -> translate(t, -centroid), tv)
-		tv .= map(t -> rotate(t, R), tv)
-		tv .= map(t -> translate(t, centroid), tv)
+        tv = @view tris[2:end]
+        centroid = centroidish(tv)
+
+        R = rotation_matrix(ℜ³(0, 0, 1), pi * 0.16)
+        tv .= map(t -> translate(t, -centroid), tv)
+        tv .= map(t -> rotate(t, R), tv)
+        tv .= map(t -> translate(t, centroid), tv)
 
 
-		BV_BISCETION_COUNT = 7
+        BV_BISCETION_COUNT = 7
 
-		bounding_volumes, bounding_volumes_members = bv_partition(tris, BV_BISCETION_COUNT; verbose=true)
+        bounding_volumes, bounding_volumes_members =
+            bv_partition(tris, BV_BISCETION_COUNT; verbose = true)
 
-		Λ = CuArray(collect(λ_min:dλ:λ_max))
-		tex_f() = checkered_tex(32, 16, length(λ_min:dλ:λ_max)) .*2#.* 12#CUDA.zeros(Float32, width ÷2, width÷2, length(Λ))
+        Λ = CuArray(collect(λ_min:dλ:λ_max))
+        tex_f() = checkered_tex(32, 16, length(λ_min:dλ:λ_max)) .* 2#.* 12#CUDA.zeros(Float32, width ÷2, width÷2, length(Λ))
 
-		basic_params = Dict{Symbol, Any}()
-		@pack! basic_params = width, height, dλ, λ_min, λ_max, depth, first_diffuse, forward_upscale, backward_upscale, iterations_per_frame
+        basic_params = Dict{Symbol,Any}()
+        @pack! basic_params = width,
+        height,
+        dλ,
+        λ_min,
+        λ_max,
+        depth,
+        first_diffuse,
+        forward_upscale,
+        backward_upscale,
+        iterations_per_frame
 
-		# Forward Trace light map
+		skydome = let 
+			a = RGBf.(load("objs/skydome2.png"))
+			R = map(rgb->rgb.r, a)
+			G = map(rgb->rgb.g, a)
+			B = map(rgb->rgb.b, a)
+			RGB = Array{Float32}(undef, size(R)..., 3)
+			RGB[:, :, 1] .= R
+			RGB[:, :, 2] .= G
+			RGB[:, :, 3] .= B
+			CuTexture(CuTextureArray(RGB); interpolation=CUDA.LinearInterpolation())
+		end 
 
-		#ray_generator(x, y, λ, dv) = simple_light(ℜ³(0, 1, 0), ℜ³(0, -1, 0), ℜ³(0, 0, 1) * 0.3, ℜ³(1, 0, 0) * 0.3, height, width, x, y, λ, dv)
-		light_size = 1024
-		lights = [
-			RectLight(ℜ³(1, 0, 0), ℜ³(-1, 0, 0), ℜ³(0, 0, 1) * 0.3, ℜ³(0, 1, 0) * 0.3, light_size, light_size),
-		]
+        # Forward Trace light map
 
-		function my_moving_camera()
-	        camera_pos = ℜ³((0, 0.8, 0))
-	        look_at = ℜ³(0, 0, 0)
-	        up = ℜ³((0.0, 0.0, 1.0))
-	        FOV = 45.0 * pi / 180.0
+        #ray_generator(x, y, λ, dv) = simple_light(ℜ³(0, 1, 0), ℜ³(0, -1, 0), ℜ³(0, 0, 1) * 0.3, ℜ³(1, 0, 0) * 0.3, height, width, x, y, λ, dv)
+        light_size = 1024
+        lights = [
+            RectLight(
+                ℜ³(1, 0, 0),
+                ℜ³(-1, 0, 0),
+                ℜ³(0, 0, 1) * 0.3,
+                ℜ³(0, 1, 0) * 0.3,
+                light_size,
+                light_size,
+            ),
+        ]
 
-	        return get_camera(camera_pos, look_at, up, FOV)
-	    end
+        function my_moving_camera()
+            camera_pos = ℜ³((0, 0.8, 0))
+            look_at = ℜ³(0, 0, 0)
+            up = ℜ³((0.0, 0.0, 1.0))
+            FOV = 45.0 * pi / 180.0
 
-	    cam = my_moving_camera()
-		#forward_hitter = ExperimentalHitter3(CuArray, light_size ^ 2 ÷ (forward_upscale ^ 2) * length(lights))#, tris, bounding_volumes, bounding_volumes_members)
-		#backward_hitter = ExperimentalHitter3(CuArray, height * width ÷ (backward_upscale ^ 2))#, tris, bounding_volumes, bounding_volumes_members)
- 		
-		forward_hitter = DPBVHitter(CuArray, light_size ^ 2 ÷ (forward_upscale ^ 2) * length(lights), tris, bounding_volumes, bounding_volumes_members; concurrency=32)
-		backward_hitter = DPBVHitter(CuArray, height * width ÷ (backward_upscale ^ 2), tris, bounding_volumes, bounding_volumes_members; concurrency=32)
- 		
-		#forward_hitter = BoundingVolumeHitter(CuArray, light_size ^ 2 ÷ (forward_upscale ^ 2) * length(lights), bounding_volumes, bounding_volumes_members)
+            return get_camera(camera_pos, look_at, up, FOV)
+        end
+
+        cam = my_moving_camera()
+        #forward_hitter = ExperimentalHitter3(CuArray, light_size ^ 2 ÷ (forward_upscale ^ 2) * length(lights))#, tris, bounding_volumes, bounding_volumes_members)
+        #backward_hitter = ExperimentalHitter3(CuArray, height * width ÷ (backward_upscale ^ 2))#, tris, bounding_volumes, bounding_volumes_members)
+		forward_hitter_A = DPBVHitter(
+			CuArray,
+			height * width ÷ (forward_upscale^2) * length(lights),
+			tris,
+			bounding_volumes,
+			bounding_volumes_members;
+			concurrency = 32,
+		)
+		backward_hitter_A = DPBVHitter(
+			CuArray,
+			height * width ÷ (backward_upscale^2),
+			tris,
+			bounding_volumes,
+			bounding_volumes_members;
+			concurrency = 32,
+		)
+		forward_hitter_B = DPBVHitter(
+			CuArray,
+			height * width ÷ (forward_upscale^2) * length(lights),
+			tris,
+			bounding_volumes,
+			bounding_volumes_members;
+			concurrency = 32,
+		)
+		backward_hitter_B = DPBVHitter(
+			CuArray,
+			height * width ÷ (backward_upscale^2),
+			tris,
+			bounding_volumes,
+			bounding_volumes_members;
+			concurrency = 32,
+		)
+	
+		#forward_hitter = ExperimentalHitter6(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights))#, tris, bounding_volumes, bounding_volumes_members)
+		#backward_hitter = ExperimentalHitter6(CuArray, height * width ÷ (backward_upscale ^ 2))#, tris, bounding_volumes, bounding_volumes_members)
+	
+		#forward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights), bounding_volumes, bounding_volumes_members)
 		#backward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (backward_upscale ^ 2), bounding_volumes, bounding_volumes_members)
- 
-		
-		runme(i) = begin
-			trace_kwargs = Dict{Symbol, Any}()
-			@pack! trace_kwargs = cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
+	
+		host_RGB_A, host_RGB_B =
+			Array{RGBf}(undef, height, width), Array{RGBf}(undef, height, width)
+		device_RGB_A, device_RGB_B = CuArray(host_RGB_A), CuArray(host_RGB_B)
+		Mem.pin(host_RGB_A)
+		Mem.pin(host_RGB_B)
+	
+		runme_A(i) = begin
+			trace_kwargs = Dict{Symbol,Any}()
+			forward_hitter, backward_hitter = forward_hitter_A, backward_hitter_A
+	
+			@pack! trace_kwargs =
+				cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
 			trace_kwargs = merge(basic_params, trace_kwargs)
-			RGB = trace!(ExperimentalTracer, ExperimentalImager2; intensity=3.0f0, trace_kwargs...)
-
-			return reshape(RGB, (height, width))
+			RGB = trace!(
+				StableTracer,
+				ExperimentalImager2;
+				intensity = 1.0f0,
+				skydome=skydome,
+				#force_rand = 1.0f0,
+				trace_kwargs...,
+			)
+			#return reshape(RGB, (height, width))
+			#copyto!(host_RGB, RGB)
+	
+			device_RGB_A = (reshape(RGB, (height, width)))
+	
 		end
-
-
+	
+	
+		runme_B(i) = begin
+			trace_kwargs = Dict{Symbol,Any}()
+			forward_hitter, backward_hitter = forward_hitter_B, backward_hitter_B
+	
+			@pack! trace_kwargs =
+				cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
+			trace_kwargs = merge(basic_params, trace_kwargs)
+			RGB = trace!(
+				StableTracer,
+				ExperimentalImager2;
+				intensity = 1.0f0,
+				skydome=skydome,
+				#force_rand = 1.0f0,
+				trace_kwargs...,
+			)
+			#return reshape(RGB, (height, width))
+			#copyto!(host_RGB, RGB)
+	
+			device_RGB_B = (reshape(RGB, (height, width)))
+	
+		end
+	
+	
 		matrix = Array{RGBf}(undef, height, width)
 		fig, ax, hm = image(1:height, 1:width, matrix)
-
-
+	
+	
 		# we use `record` to show the resulting video in the docs.
 		# If one doesn't need to record a video, a normal loop works as well.
 		# Just don't forget to call `display(fig)` before the loop
 		# and without record, one needs to insert a yield to yield to the render task
-
-		runme(1)
-		runme(1)
-		hm[3] = runme(1)
+	
+		runme_A(1)
+		runme_B(2)
+		hm[3] = runme_A(1)
 		display(fig)
-		tri_copy = copy(tris)
-		bvs2, bvms2 = copy(bounding_volumes), copy(bounding_volumes_members)
-		@time for i in 1:400
-
-		#    events(hm).mouseposition |> println
-			 recalc_task = Threads.@spawn begin
-				tri_copy = copy(tris) 
-				tv = @view tri_copy[2:end]
-				#oscillate(tv) = translate(tv, ℜ³(cos(i / 20) / 500, 0, sin(i / 20) / 500))
-				#tv .= oscillate.(tv)
-				
-				centroid = centroidish(tv)
-					
-				R = rotation_matrix(ℜ³(0, 1, 0), 2 * pi * 1 / 400)
-				tv .= map(t -> translate(t, -centroid), tv)
-				tv .= map(t -> rotate(t, R), tv)
-				tv .= map(t -> translate(t, centroid), tv)
-
-				bvs2, bvms2 = bv_partition(tri_copy, BV_BISCETION_COUNT)
-			
-			end 
-			
-			hm[3] = runme(1) # update data
-			wait(recalc_task)
-			tris .= tri_copy
-			repack!(forward_hitter, bvs2, bvms2)
-			repack!(backward_hitter, bvs2, bvms2)
+	
+		tasks = Dict()
+	
+		t0 = @async 1
+		tasks["A"] = t0
+	
+		@time for i = 1:40
+			#    events(hm).mouseposition |> println
+	
+			if i % 2 == 1
+				# assume RBG_A ready, get B ready for next loop
+				tasks["B"] = @async begin
+					runme_B(i)
+					copyto!(host_RGB_B, device_RGB_B)
+				end#host_RGB_B = Array(device_RGB_B)
+	
+				wait(tasks["A"])
+				#Threads.@spawn save("out/$i.png", host_RGB_A)	
+	
+				hm[3] = host_RGB_A
+			else
+				tasks["A"] = @async begin
+					runme_A(i)
+					copyto!(host_RGB_A, device_RGB_A)
+				end#host_RGB_A = Array(device_RGB_A)
+	
+				wait(tasks["B"])
+				#Threads.@spawn save("out/$i.png", host_RGB_B)
+				hm[3] = host_RGB_B
+			end
+	
 			yield()
-			
-		end
-	end
-	return nothing
+			#@async save("out/$i.png", fig )
+		
+    end
+    return nothing
 end
 #try
 
--- src/scenes/sphere.jl
+++ src/scenes/sphere.jl
@@ -1,5 +1,5 @@
 # RUN FROM /
-using Revise, LazyArrays, Parameters, GLMakie, CUDA,  NVTX
+using Revise, LazyArrays, Parameters, GLMakie, CUDA, NVTX
 
 include("../geo.jl")
 include("../skys.jl")
@@ -7,7 +7,7 @@ include("../tracer.jl")
 include("../procedural_assets.jl")
 
 function main()
-	# Tracing params
+    # Tracing params
     width = 1280
     height = 720
     dλ = 25.0f0
@@ -15,34 +15,42 @@ function main()
     λ_max = 700.0f0
     depth = 3
     ITERS = 1
-	forward_upscale = 4
-	backward_upscale = 4
-	# Geometry
+    forward_upscale = 2
+    backward_upscale = 2
+    # Geometry
 
-	obj_path = "objs/icos.obj"
-	
-	glass_sphere = mesh_to_FTri(load(obj_path))
-	
-	
-	solid_sphere = mesh_to_FTri(load(obj_path))
-	solid_sphere = map(t -> translate(t, ℜ³(1, 0, -4) ), solid_sphere)
-	
-	glass_sphere2 = mesh_to_FTri(load(obj_path))
-	glass_sphere2 = map(t -> translate(t, ℜ³(0, 2, 0) ), glass_sphere2)
-	
-	solid_sphere2 = mesh_to_FTri(load(obj_path))
-	solid_sphere2 = map(t -> translate(t, ℜ³(1, 2, -4) ), solid_sphere2)
+    obj_path = "objs/icos.obj"
 
-	meshes = [[zero(FTri)], glass_sphere, glass_sphere2, solid_sphere, solid_sphere2]
-	first_diffuse = 1 + 1 + length(glass_sphere) * 2
-	host_tris = foldl(vcat, meshes)
-	tris = CuArray(host_tris)
-	
+    glass_sphere = mesh_to_FTri(load(obj_path))
 
-	tex_f() = checkered_tex(32, 16, length(λ_min:dλ:λ_max)) .* 12
 
-	basic_params = Dict{Symbol, Any}()
-	@pack! basic_params = width, height, dλ, λ_min, λ_max, depth, first_diffuse, forward_upscale, backward_upscale
+    solid_sphere = mesh_to_FTri(load(obj_path))
+    solid_sphere = map(t -> translate(t, ℜ³(1, 0, -4)), solid_sphere)
+
+    glass_sphere2 = mesh_to_FTri(load(obj_path))
+    glass_sphere2 = map(t -> translate(t, ℜ³(0, 2, 0)), glass_sphere2)
+
+    solid_sphere2 = mesh_to_FTri(load(obj_path))
+    solid_sphere2 = map(t -> translate(t, ℜ³(1, 2, -4)), solid_sphere2)
+
+    meshes = [[zero(FTri)], glass_sphere, glass_sphere2, solid_sphere, solid_sphere2]
+    first_diffuse = 1 + 1 + length(glass_sphere) * 2
+    host_tris = foldl(vcat, meshes)
+    tris = CuArray(host_tris)
+
+
+    tex_f() = checkered_tex(32, 16, length(λ_min:dλ:λ_max)) .* 12
+
+    basic_params = Dict{Symbol,Any}()
+    @pack! basic_params = width,
+    height,
+    dλ,
+    λ_min,
+    λ_max,
+    depth,
+    first_diffuse,
+    forward_upscale,
+    backward_upscale
 
     function my_moving_camera()
         camera_pos = ℜ³((0, 5, 5))
@@ -55,69 +63,161 @@ function main()
 
     cam = my_moving_camera()
 
-	lights = [
-		RectLight(ℜ³(0, 0, 8), ℜ³(0, 0, -1), ℜ³(1, 0, 0), ℜ³(0, 1, 0), height, width),
-	]
+    lights = [RectLight(ℜ³(0, 0, 8), ℜ³(0, 0, -1), ℜ³(1, 0, 0), ℜ³(0, 1, 0), height, width)]
 
-	#bounding_volumes, bounding_volumes_members = cluster_fuck(Array(tris), 4)
+    #bounding_volumes, bounding_volumes_members = cluster_fuck(Array(tris), 4)
 
-	bounding_volumes, bounding_volumes_members = bv_partition(tris, 5; verbose=true)
+    bounding_volumes, bounding_volumes_members = bv_partition(tris, 5; verbose = true)
 
-	forward_hitter = DPBVHitter(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights), tris, bounding_volumes, bounding_volumes_members; concurrency=32)
-    backward_hitter = DPBVHitter(CuArray, height * width ÷ (backward_upscale ^ 2), tris, bounding_volumes, bounding_volumes_members; concurrency=32)
-    
-	#forward_hitter = ExperimentalHitter6(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights))#, tris, bounding_volumes, bounding_volumes_members)
+    forward_hitter_A = DPBVHitter(
+        CuArray,
+        height * width ÷ (forward_upscale^2) * length(lights),
+        tris,
+        bounding_volumes,
+        bounding_volumes_members;
+        concurrency = 32,
+    )
+    backward_hitter_A = DPBVHitter(
+        CuArray,
+        height * width ÷ (backward_upscale^2),
+        tris,
+        bounding_volumes,
+        bounding_volumes_members;
+        concurrency = 32,
+    )
+    forward_hitter_B = DPBVHitter(
+        CuArray,
+        height * width ÷ (forward_upscale^2) * length(lights),
+        tris,
+        bounding_volumes,
+        bounding_volumes_members;
+        concurrency = 32,
+    )
+    backward_hitter_B = DPBVHitter(
+        CuArray,
+        height * width ÷ (backward_upscale^2),
+        tris,
+        bounding_volumes,
+        bounding_volumes_members;
+        concurrency = 32,
+    )
+
+    #forward_hitter = ExperimentalHitter6(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights))#, tris, bounding_volumes, bounding_volumes_members)
     #backward_hitter = ExperimentalHitter6(CuArray, height * width ÷ (backward_upscale ^ 2))#, tris, bounding_volumes, bounding_volumes_members)
-   
-	#forward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights), bounding_volumes, bounding_volumes_members)
-	#backward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (backward_upscale ^ 2), bounding_volumes, bounding_volumes_members)
- 
-	host_RGB_A, host_RGB_B = Array{RGBf}(undef, height, width), Array{RGBf}(undef, height, width)
-	device_RGB_A, device_RGB_B = CuArray(host_RGB_A), CuArray(host_RGB_B)
-	Mem.pin(host_RGB_A)
-	Mem.pin(host_RGB_B)
-	
-	runme(i) = begin
-		trace_kwargs = Dict{Symbol, Any}()
-		@pack! trace_kwargs = cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
-		trace_kwargs = merge(basic_params, trace_kwargs)
-		RGB = trace!(StableTracer, ExperimentalImager2; intensity=1.0f0, force_rand=1f0, trace_kwargs...)
-		#return reshape(RGB, (height, width))
-		#copyto!(host_RGB, RGB)
-		
-			device_RGB_A = (reshape(RGB, (height, width)))
-		
-	end
+
+    #forward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (forward_upscale ^ 2) * length(lights), bounding_volumes, bounding_volumes_members)
+    #backward_hitter = BoundingVolumeHitter(CuArray, height * width ÷ (backward_upscale ^ 2), bounding_volumes, bounding_volumes_members)
+
+    host_RGB_A, host_RGB_B =
+        Array{RGBf}(undef, height, width), Array{RGBf}(undef, height, width)
+    device_RGB_A, device_RGB_B = CuArray(host_RGB_A), CuArray(host_RGB_B)
+    Mem.pin(host_RGB_A)
+    Mem.pin(host_RGB_B)
+    skydome = let 
+        a = RGBf.(load("objs/skydome2.png"))
+        R = map(rgb->rgb.r, a)
+        G = map(rgb->rgb.g, a)
+        B = map(rgb->rgb.b, a)
+        RGB = Array{Float32}(undef, size(R)..., 3)
+        RGB[:, :, 1] .= R
+        RGB[:, :, 2] .= G
+        RGB[:, :, 3] .= B
+        CuTexture(CuTextureArray(RGB); interpolation=CUDA.LinearInterpolation())
+    end 
+    runme_A(i) = begin
+        trace_kwargs = Dict{Symbol,Any}()
+        forward_hitter, backward_hitter = forward_hitter_A, backward_hitter_A
+
+        @pack! trace_kwargs =
+            cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
+        trace_kwargs = merge(basic_params, trace_kwargs)
+        RGB = trace!(
+            StableTracer,
+            ExperimentalImager2;
+            intensity = 1.0f0,
+            force_rand = 0.0f0,
+            skydome=skydome,
+            trace_kwargs...,
+        )
+        #return reshape(RGB, (height, width))
+        #copyto!(host_RGB, RGB)
+
+        device_RGB_A = (reshape(RGB, (height, width)))
+
+    end
 
 
-	matrix = Array{RGBf}(undef, height, width)
-	fig, ax, hm = image(1:height, 1:width, matrix)
+    runme_B(i) = begin
+        trace_kwargs = Dict{Symbol,Any}()
+        forward_hitter, backward_hitter = forward_hitter_B, backward_hitter_B
+
+        @pack! trace_kwargs =
+            cam, lights, tex_f, tris, λ_min, dλ, λ_max, forward_hitter, backward_hitter
+        trace_kwargs = merge(basic_params, trace_kwargs)
+        RGB = trace!(
+            StableTracer,
+            ExperimentalImager2;
+            intensity = 1.0f0,
+            #force_rand = 1.0f0,
+            skydome=skydome,
+            trace_kwargs...,
+        )
+        #return reshape(RGB, (height, width))
+        #copyto!(host_RGB, RGB)
+
+        device_RGB_B = (reshape(RGB, (height, width)))
+
+    end
 
 
-	# we use `record` to show the resulting video in the docs.
-	# If one doesn't need to record a video, a normal loop works as well.
-	# Just don't forget to call `display(fig)` before the loop
-	# and without record, one needs to insert a yield to yield to the render task
+    matrix = Array{RGBf}(undef, height, width)
+    fig, ax, hm = image(1:height, 1:width, matrix)
 
-	runme(1)
-	runme(2)
-	hm[3] = runme(1)
-	display(fig)
 
-	tasks = Dict()
+    # we use `record` to show the resulting video in the docs.
+    # If one doesn't need to record a video, a normal loop works as well.
+    # Just don't forget to call `display(fig)` before the loop
+    # and without record, one needs to insert a yield to yield to the render task
 
-	t0 = @async 1
-	tasks["A"] = t0
+    runme_A(1)
+    runme_B(2)
+    hm[3] = runme_A(1)
+    display(fig)
 
-	@time for i in 1:40
-	#    events(hm).mouseposition |> println
-		
-			# assume RBG_A ready, get B ready for next loop
-			runme(i); copyto!(host_RGB_A, device_RGB_A)
-			hm[3] = host_RGB_A
-		
-		yield()
-		#@async save("out/$i.png", fig )
-	end
+    tasks = Dict()
+
+    t0 = @async 1
+    tasks["A"] = t0
+
+    @time for i = 1:40
+        #    events(hm).mouseposition |> println
+
+        if i % 2 == 1
+            # assume RBG_A ready, get B ready for next loop
+            tasks["B"] = @async begin
+                runme_B(i)
+                copyto!(host_RGB_B, device_RGB_B)
+            end#host_RGB_B = Array(device_RGB_B)
+
+            wait(tasks["A"])
+            #Threads.@spawn save("out/$i.png", host_RGB_A)	
+
+        #    hm[3] = host_RGB_A
+        else
+            tasks["A"] = @async begin
+                runme_A(i)
+                copyto!(host_RGB_A, device_RGB_A)
+            end#host_RGB_A = Array(device_RGB_A)
+
+            wait(tasks["B"])
+            #Threads.@spawn save("out/$i.png", host_RGB_B)
+            #hm[3] = host_RGB_B
+        end
+
+        yield()
+        #@async save("out/$i.png", fig )
+    end
 end
 CUDA.@profile main()
+
+
